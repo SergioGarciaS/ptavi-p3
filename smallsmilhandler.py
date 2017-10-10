615f0697009ed76810628073a347f6f72ba93558
Garcias <sergio.garcia.sti@gmail.com>
2017-10-10 16:41:37 +0200

    creado el esquema de las etiquetas

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,35 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
+class SmallSMILHandler(ContentHandler):
+
+    def __init__ (self):
+        self.root_layout = {"width": "", "height": "","background_color" : ""};
+        self.region = {"id": "","top": "", "bottom" : "","left":"","right":""};
+        self.img = {"src":"","region": "","begin" : "", "dur" : ""};
+        self.audio = {"src":"","begin" : "", "dur" : ""};
+        self.textstream = {"src":"","region": ""};
+    
+    def startElement(self,name,attrs):
+        if name == 'root_layout':
+            #Captura la etiqueta y guarda los atributos si los hubiese.
+        if name == 'region':
+            #Captura la etiqueta y guarda los atributos si los hubiese.
+        if name == 'img':
+            #Captura la etiqueta y guarda los atributos si los hubiese.
+        if name == 'audio':
+            #Captura la etiqueta y guarda los atributos si los hubiese.
+        if name == 'textstream':
+            #Captura la etiqueta y guarda los atributos si los hubiese.
+   
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = ChistesHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('chistes2.xml'))
