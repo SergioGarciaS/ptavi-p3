7a3c85249285a551d5ab7908145c35768a5b1866
Garcias <sergio.garcia.sti@gmail.com>
2017-10-11 17:23:26 +0200

    creación correcta de la estructura

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -7,36 +7,47 @@ from xml.sax.handler import ContentHandler
 class SmallSMILHandler(ContentHandler):
 
     def __init__ (self):
-    
-        self.root_layout = {"width": "", "height": "","background_color" : ""}
-        self.region = {"id": "","top": "", "bottom" : "","left":"","right":""}
-        self.img = {"src":"","region": "","begin" : "", "dur" : ""}
-        self.audio = {"src":"","begin" : "", "dur" : ""}
-        self.textstream = {"src":"","region": ""}
+        self.motherlist = []
+        self.dic = { 'root_layout' : ["width", "height","background_color"], 
+        'region' : ['id', 'top', 'bottom', 'left','right'], 
+        'img': ['src', 'region','begin','dur'], 
+        'audio': ['src','dur'], 'textstream' : ['src','region']}
     
     def startElement(self,name,attrs):
-        atributos = name.keys()
+
         if name == 'root_layout':
             for i in atributos:
-                self.root_layout[i] = attrs.get('i', "")
+                self.root_layout['i'] = attrs.get('i', "")
+            self.motherlist.append(self.root_layout)
         if name == 'region':
             for i in atributos:
-                self.region[i] = attrs.get('i', "")
+                self.region['i'] = attrs.get('i', "")
+            self.motherlist.append(self.region)
         if name == 'img':
             for i in atributos:
-                self.img[i] = attrs.get('i', "")
+                self.img['i'] = attrs.get('i', "")
+            self.motherlist.append(self.img)
         if name == 'audio':
             for i in atributos:
-                self.audio[i] = attrs.get('i', "")
+                self.audio['i'] = attrs.get('i', "")
+            self.motherlist.append(self.audio)
         if name == 'textstream':
             for i in atributos:
-                self.textstream[i] = attrs.get('i', "")
+                self.textstream['i'] = attrs.get('i', "")
+            self.motherlist.append(self.textstream)
+            
+    def get_tags(self):
+        return self.mortherlist
+               
+        
    
 if __name__ == "__main__":
     """
     Programa principal
     """
     parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
+    KJandler = SmallSMILHandler()
+    parser.setContentHandler(KJandler)
     parser.parse(open('karaoke.smil'))
+    
+    print(KJandler.get_tags())
