8e1114ce69dec1c68b27dc79087d0a7a13389643
Garcias <sergio.garcia.sti@gmail.com>
2017-10-16 10:59:46 +0200

    implementado el formato de listado de etiquetas

--- karaoke.py
+++ karaoke.py
@@ -7,16 +7,34 @@ import json
 from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
 
-class karaokelocal:
-    def __init__(self,file):
+class karaokelocal(SmallSMILHandler):
+    def __init__(self):
+        try:
+            fich = sys.argv[1]
+        except IndexError:
+            sys.exit("Usage: python3 karaoke.py file.smil")
         parser = make_parser()
         KHandler = SmallSMILHandler()
         parser.setContentHandler(KHandler)
-        parser.parse(open(file))
+        parser.parse(open(fich))
         self.mytags = KHandler.get_tags()
 
+    def format(self):
+        jambor = ""
+        for dic in self.mytags:
+            #print(dic)
+            frase = "\t"
+
+            for element in dic:
+                #print(element)
+                for atributo in dic[element]:
+                    #print(atributo, dic[element][atributo])
+                    frase  += atributo + " = "+ dic[element][atributo] + "\t"
+                jambor += element + frase + "\n"
+        return jambor
+
 if __name__ == "__main__":
-    try:
-        fich = karaokelocal(sys.argv[1])
-    except IndexError:
-        sys.exit("Usage: python3 karaoke.py file.smil")
+
+
+    karaok = karaokelocal()
+    print(karaok.format())
